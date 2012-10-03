e9a57371ea9d9c599d1ba37f89d2750f19608a40
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-10-03 18:07:56 +0200

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class TestCommand(Command):
         t.run(tests)
 
 setup(name='rfc3161',
-        version='0.1.4',
+        version='0.1.5',
         license='MIT',
         description='Python implementation of the RFC3161 specification, using pyasn1',
         author='Benjamin Dauvergne',
