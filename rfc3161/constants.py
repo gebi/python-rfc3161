800b5ed8786fa75c180532577a4b8831fd278eb3
Michael Gebetsroither <michael@mgeb.org>
2012-10-03 16:45:20 +0200

    add support for sha256, sha384 and sha512

--- rfc3161/constants.py
+++ rfc3161/constants.py
@@ -1,7 +1,10 @@
 from pyasn1.type import univ
 
-__all__ = ('id_kp_timeStamping','id_sha1', 'id_ct_TSTInfo',)
+__all__ = ('id_kp_timeStamping','id_sha1', 'id_sha256', 'id_sha384', 'id_sha512', 'id_ct_TSTInfo',)
 
 id_kp_timeStamping = univ.ObjectIdentifier((1,3,6,1,5,5,7,3,8))
 id_sha1 = univ.ObjectIdentifier((1,3,14,3,2,26))
+id_sha256 = univ.ObjectIdentifier((2,16,840,1,101,3,4,2,1,))
+id_sha384 = univ.ObjectIdentifier((2,16,840,1,101,3,4,2,2,))
+id_sha512 = univ.ObjectIdentifier((2,16,840,1,101,3,4,2,3,))
 id_ct_TSTInfo = univ.ObjectIdentifier((1,2,840,113549,1,9,16,1,4))
